d697559051ec28a08512d6272016db74981fb6d4
hbdgr <hbdgr@protonmail.com>
2020-09-26 20:02:30 +0200

    separate config

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Eth struct {
+		// Infura http url
+		InfuraURL string `yaml:"infura_url"`
+	} `yaml:"eth"`
+}
+
+// NewConfig returns a new decoded Config struct
+func NewConfig(configPath string) (*Config, error) {
+	config := &Config{}
+
+	// Open config file
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Init new YAML decode
+	d := yaml.NewDecoder(file)
+
+	// Start YAML decoding from file
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
--- main.go
+++ main.go
@@ -1,51 +1,14 @@
 package main
 
 import (
+	"ens_feed/config"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"os"
-
-	"gopkg.in/yaml.v2"
 )
-
-type Config struct {
-	Eth struct {
-		// Infura http url
-		InfuraURL string `yaml:"infura_url"`
-	} `yaml:"eth"`
-}
-
-// NewConfig returns a new decoded Config struct
-func NewConfig(configPath string) (*Config, error) {
-	config := &Config{}
-
-	// Open config file
-	file, err := os.Open(configPath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// Init new YAML decode
-	d := yaml.NewDecoder(file)
-
-	// Start YAML decoding from file
-	if err := d.Decode(&config); err != nil {
-		return nil, err
-	}
-
-	return config, nil
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
-}
 
 func main() {
 	cfgPath := "./config.yml"
-	cfg, err := NewConfig(cfgPath)
+	cfg, err := config.NewConfig(cfgPath)
 	if err != nil {
 		log.Fatal(err)
 	}
